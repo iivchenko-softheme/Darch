2312f0fe8bc0243a455db558e0b3a15ce1268776
iivchenko <ivan.ivchenko@softheme.com>
2015-03-09 20:12:27 +0200

    Implement StreamRepository
    Add IStreamMapper

---
+++ RepositoryLib/IStreamMapper.cs
@@ -0,0 +1,15 @@
+﻿// <copyright company="XATA">
+//      Copyright (c) 2015, All Right Reserved
+// </copyright>
+// <author>Ivan Ivchenko</author>
+// <email>iivchenko@live.com</email>
+
+namespace RepositoryLib
+{
+    public interface IStreamMapper<TItem>
+    {
+        byte[] Convert(TItem item);
+
+        TItem Convert(byte[] buffer);
+    }
+}
--- RepositoryLib/StreamRepository.cs
+++ RepositoryLib/StreamRepository.cs
@@ -4,34 +4,182 @@
 // <author>Ivan Ivchenko</author>
 // <email>iivchenko@live.com</email>
 
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace RepositoryLib
 {
-    public class StreamRepository<TItem, TId> : IRepository<TItem, TId>
+    // TODO: Create documentation
+    // TODO: Implement integratioon tests
+    // TODO: Implement performance tests
+    // TODO: Implement item size validation
+    // TODO: Check algorithms
+    public sealed class StreamRepository<TItem> : IRepository<TItem, uint>
     {
-        public uint Count { get; private set; }
+        private const int IdSize = sizeof(int);
 
-        public IEnumerable<TItem> All { get; private set; }
+        private readonly Stream _stream;
+        private readonly IStreamMapper<TItem> _streamMapper;
+        private readonly int _itemSize;
+
+        private readonly Lazy<IDictionary<uint, long>> _cache;
         
-        public TItem GetById(TId id)
+        public StreamRepository(
+            Stream stream,
+            IStreamMapper<TItem> streamMapper,
+            int itemSize)
         {
-            throw new System.NotImplementedException();
+            _stream = stream;
+            _streamMapper = streamMapper;
+            _itemSize = itemSize;
+
+            _cache = new Lazy<IDictionary<uint, long>>(() =>
+                                                      {
+                                                          var cache = new Dictionary<uint, long>();
+                                                          long index = 0;
+
+                                                          while (index < _stream.Length)
+                                                          {
+                                                              var id = ReadId(index);
+
+                                                              cache.Add(id, index);
+
+                                                              index += IdSize + _itemSize;
+                                                          }
+
+                                                          return cache;
+                                                      });
+        }
+        
+        public uint Count
+        {
+            get { return (uint)_stream.Length / (uint)(IdSize + _itemSize); }
         }
 
-        public TId Add(TItem item)
+        public IEnumerable<TItem> All
         {
-            throw new System.NotImplementedException();
+            get
+            {
+                // TODO: Check speed for the algorithm
+                return _cache.Value.Keys.Select(id => ReadItem(GetItemIndex(id)));
+            }
         }
 
-        public void Delete(TId id)
+        public TItem GetById(uint id)
         {
-            throw new System.NotImplementedException();
+            ThrowIfItemNotExists(id);
+
+            return ReadItem(GetItemIndex(id));
         }
 
-        public void Update(TId id, TItem item)
+        public uint Add(TItem item)
         {
-            throw new System.NotImplementedException();
+            var newId = _cache.Value.Keys.Max() + 1;
+            var newIndex = _stream.Length;
+
+            WriteIdAndItem(newIndex, newId, item);
+
+            _cache.Value.Add(newId, newIndex);
+
+            return newId;
+        }
+
+        public void Delete(uint id)
+        {
+            ThrowIfItemNotExists(id);
+
+            var indexToDelete = _cache.Value[id];
+            var indexToMove = _stream.Length - (IdSize + _itemSize);
+
+            _cache.Value.Remove(id);
+
+            // In case if we delete only one element
+            if (indexToDelete == indexToMove)
+            {
+                _stream.SetLength(_stream.Length - (IdSize + _itemSize));
+                return;
+            }
+
+            var idToMove = ReadId(indexToMove);
+            var itemToMove = ReadItem(indexToMove + IdSize);
+
+            WriteIdAndItem(indexToDelete, idToMove, itemToMove);
+
+            _stream.SetLength(_stream.Length - (IdSize + _itemSize));
+        }
+
+        public void Update(uint id, TItem item)
+        {
+            ThrowIfItemNotExists(id);
+
+            WriteItem(GetItemIndex(id), item);
+        }
+
+        private void ThrowIfItemNotExists(uint id)
+        {
+            if (!_cache.Value.ContainsKey(id))
+            {
+                // TODO: Implement
+                throw new NotImplementedException();
+            }
+        }
+
+        private TItem ReadItem(long index)
+        {
+            var buffer = new byte[_itemSize];
+
+            _stream.Seek(index, SeekOrigin.Begin);
+            var bytesRead = _stream.Read(buffer, 0, _itemSize);
+
+            if (bytesRead != _itemSize)
+            {
+                // TODO: throw
+            }
+
+            return _streamMapper.Convert(buffer);
+        }
+
+        private uint ReadId(long index)
+        {
+            var buffer = new byte[IdSize];
+
+            _stream.Seek(index, SeekOrigin.Begin);
+            var bytesRead = _stream.Read(buffer, 0, IdSize);
+
+            if (bytesRead != IdSize)
+            {
+                // TODO: throw
+            }
+
+            return BitConverter.ToUInt32(buffer, 0);
+        }
+
+        private void WriteIdAndItem(long index, uint id, TItem item)
+        {
+            var buffer = BitConverter.GetBytes(id);
+
+            _stream.Seek(index, SeekOrigin.Begin);
+
+            _stream.Write(buffer, 0, _itemSize);
+
+            buffer = _streamMapper.Convert(item);
+
+            _stream.Write(buffer, 0, _itemSize);
+        }
+
+        private void WriteItem(long index, TItem item)
+        {
+            var buffer = _streamMapper.Convert(item);
+
+            _stream.Seek(index, SeekOrigin.Begin);
+            _stream.Write(buffer, 0, _itemSize);
+        }
+
+        private long GetItemIndex(uint id)
+        {
+            return _cache.Value[id] + IdSize;
         }
     }
 }
