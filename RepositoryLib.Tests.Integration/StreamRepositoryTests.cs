5320016bbdfa175e84cc15c02b1600957605712f
iivchenko <ivan.ivchenko@softheme.com>
2015-03-09 14:52:36 +0200

    Add StreamRepository
    Add StreamRepository class
    Add StreamRepositoryTests
    Add Moq and NUnit to the tests

---
+++ RepositoryLib.Tests.Integration/StreamRepositoryTests.cs
@@ -0,0 +1,15 @@
+﻿// <copyright company="XATA">
+//      Copyright (c) 2015, All Right Reserved
+// </copyright>
+// <author>Ivan Ivchenko</author>
+// <email>iivchenko@live.com</email>
+
+using NUnit.Framework;
+
+namespace RepositoryLib.Tests.Integration
+{
+    [TestFixture]
+    public class StreamRepositoryTests
+    {
+    }
+}
---
+++ RepositoryLib/StreamRepository.cs
@@ -0,0 +1,47 @@
+﻿// <copyright company="XATA">
+//      Copyright (c) 2015, All Right Reserved
+// </copyright>
+// <author>Ivan Ivchenko</author>
+// <email>iivchenko@live.com</email>
+
+using System.Collections.Generic;
+
+namespace RepositoryLib
+{
+    public class StreamRepository<TItem, TId> : IRepository<TItem, TId>
+    {
+        public uint Count { get; private set; }
+
+        public IEnumerable<TItem> All { get; private set; }
+        
+        public TItem GetById(TId id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public TId Add(TItem item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(TItem item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(TId id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(TItem oldItem, TItem newItem)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(TId id, TItem item)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
