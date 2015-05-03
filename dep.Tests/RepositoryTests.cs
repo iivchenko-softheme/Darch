3a05d75df2214e5f54c7b5bf4836cf3bc93fc852
iivchenko <ivan.ivchenko@softheme.com>
2015-05-03 17:37:15 +0300

    Add Delete test

--- dep.Tests/RepositoryTests.cs
+++ dep.Tests/RepositoryTests.cs
@@ -6,6 +6,7 @@
 
 using System;
 using System.IO;
+using System.Linq;
 using Deduplication.Tests.Repository;
 using NUnit.Framework;
 
@@ -57,6 +58,46 @@ namespace Deduplication.Tests
             }
 
             Assert.AreEqual(sourceStream, targetStream);
+        }
+
+        [Test]
+        [Description("Add some maps. Delete on of them. Check if deleted map is no longer available.")]
+        public void Delete_Test()
+        {
+            var dataGenerator = new DataGenerator(_seed);
+            var sourceStream = dataGenerator.GenerateData(SourceStreamSize);
+
+            using (var repo = new ManagedRepository(BlockSize, ChecksumSize))
+            {
+                for (var i = 0; i < 3; i++)
+                {
+                    sourceStream.Seek(0, SeekOrigin.Begin);
+                    using (var map = repo.Write(sourceStream))
+                    using (new MapMonitor(map))
+                    {
+                        map.Start();
+                        map.Wait();
+                    }
+
+                    Console.WriteLine();
+                }
+
+                Assert.Contains(0, repo.Maps.ToList());
+                Assert.Contains(1, repo.Maps.ToList());
+                Assert.Contains(2, repo.Maps.ToList());
+
+                using (var map = repo.Delete(1))
+                using (new MapMonitor(map))
+                {
+                    map.Start();
+                    map.Wait();
+                }
+
+                Assert.Contains(0, repo.Maps.ToList());
+                Assert.Contains(2, repo.Maps.ToList());
+
+                Assert.IsFalse(repo.Maps.Any(x => x == 1), "Map with id 1 should be deleted!");
+            }
         }
     }
 }
