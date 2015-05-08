72726d3b4212c117cb6fa3fb2e09fc5a110f2a0a
iivchenko <ivan.ivchenko@softheme.com>
2015-05-08 22:59:07 +0300

    Add EnumerableExtensions

---
+++ Shogun.Utility.Tests/Extensions/EnumerableExtensionsTests.cs
@@ -0,0 +1,15 @@
+﻿// <copyright company="XATA">
+//      Copyright (c) 2015, All Right Reserved
+// </copyright>
+// <author>Ivan Ivchenko</author>
+// <email>iivchenko@live.com</email>
+
+using NUnit.Framework;
+
+namespace Shogun.Utility.Tests.Extensions
+{
+    [TestFixture]
+    public class EnumerableExtensionsTests
+    {
+    }
+}
---
+++ Shogun.Utility/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿// <copyright company="XATA">
+//      Copyright (c) 2015, All Right Reserved
+// </copyright>
+// <author>Ivan Ivchenko</author>
+// <email>iivchenko@live.com</email>
+
+using System.Collections.Generic;
+
+namespace Shogun.Utility.Extensions
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="IEnumerable{T}"/>
+    /// </summary>
+    public static class EnumerableExtensions
+    {
+    }
+}
