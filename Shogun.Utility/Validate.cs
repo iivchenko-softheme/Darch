a1ef1074c2c0fc56f6cda1401d51cd4aaa7de210
iivchenko <ivan.ivchenko@softheme.com>
2015-05-08 21:12:18 +0300

    Add Validate class and Tests

---
+++ Shogun.Utility/Validate.cs
@@ -0,0 +1,15 @@
+﻿// <copyright company="XATA">
+//      Copyright (c) 2015, All Right Reserved
+// </copyright>
+// <author>Ivan Ivchenko</author>
+// <email>iivchenko@live.com</email>
+
+namespace Shogun.Utility
+{
+    /// <summary>
+    /// Provides parameters/arguments validation functionality.
+    /// </summary>
+    public static class Validate
+    {
+    }
+}
