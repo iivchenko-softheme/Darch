172687890d8b3649eb128c3a7f699c70658e21e8
iivchenko <ivan.ivchenko@softheme.com>
2015-05-08 21:23:06 +0300

    Extend Shogun.Utility.Validate
    Add Null Check method

--- Shogun.Utility.Tests/ValidateTests.cs
+++ Shogun.Utility.Tests/ValidateTests.cs
@@ -4,6 +4,7 @@
 // <author>Ivan Ivchenko</author>
 // <email>iivchenko@live.com</email>
 
+using System;
 using NUnit.Framework;
 
 namespace Shogun.Utility.Tests
@@ -11,5 +12,27 @@ namespace Shogun.Utility.Tests
     [TestFixture]
     public class ValidateTests
     {
+        [Test]
+        public void Null_ParameterIsNotNull_DoNothing()
+        {
+            var parameter = new object();
+            Validate.Null(parameter, "parameter");
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Null_ParameterIsNull_Throws()
+        {
+            var parameter = (object)null;
+            Validate.Null(parameter, "parameter");
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Null_NameNotSpecified_Throws()
+        {
+            var parameter = new object();
+            Validate.Null(parameter, null);
+        }
     }
 }
--- Shogun.Utility/Validate.cs
+++ Shogun.Utility/Validate.cs
@@ -4,6 +4,8 @@
 // <author>Ivan Ivchenko</author>
 // <email>iivchenko@live.com</email>
 
+using System;
+
 namespace Shogun.Utility
 {
     /// <summary>
@@ -11,5 +13,23 @@ namespace Shogun.Utility
     /// </summary>
     public static class Validate
     {
+        /// <summary>
+        /// Throws if input is Null.
+        /// </summary>
+        /// <param name="input">Object that is under validation.</param>
+        /// <param name="parameterName">Name of the <see cref="input"/> variable/parameter. Can't be NULL.</param>
+        /// <exception cref="ArgumentNullException"/>
+        public static void Null(object input, string parameterName)
+        {
+            if (parameterName == null)
+            {
+                throw new ArgumentNullException("parameterName");
+            }
+
+            if (input == null)
+            {
+                throw new ArgumentNullException(parameterName);
+            }
+        }
     }
 }
