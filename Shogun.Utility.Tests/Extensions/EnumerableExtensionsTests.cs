47efdf4ece965188cc357e6d064c44a4a530b3e7
iivchenko <ivan.ivchenko@softheme.com>
2015-05-10 16:54:23 +0300

    Extend EnumerableExtensions
    Add IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Func<T, T> action)

--- Shogun.Utility.Tests/Extensions/EnumerableExtensionsTests.cs
+++ Shogun.Utility.Tests/Extensions/EnumerableExtensionsTests.cs
@@ -5,6 +5,7 @@
 // <email>iivchenko@live.com</email>
 
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using Shogun.Utility.Extensions;
 
@@ -14,12 +15,25 @@ namespace Shogun.Utility.Tests.Extensions
     public class EnumerableExtensionsTests
     {
         [Test]
-        public void ForEach_Test()
+        public void ForEach_WithoutResult_Test()
         {
             var counter = 1;
             IEnumerable<int> source = new List<int> { 1, 2, 3, 4, 5 };
 
             source.ForEach(x => Assert.AreEqual(counter++, x));
+        }
+
+        [Test]
+        public void ForEach_WithResult_Test()
+        {
+            IEnumerable<int> source = new List<int> { 1, 2, 3, 4, 5 };
+
+            var actual = source.ForEach(x => ++x);
+
+            for (var i = 0; i < actual.Count(); i++)
+            {
+                Assert.AreEqual(source.ElementAt(i) + 1, actual.ElementAt(i));
+            }
         }
     }
 }
--- Shogun.Utility/Extensions/EnumerableExtensions.cs
+++ Shogun.Utility/Extensions/EnumerableExtensions.cs
@@ -6,6 +6,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace Shogun.Utility.Extensions
 {
@@ -26,6 +27,25 @@ namespace Shogun.Utility.Extensions
             {
                 action(item);
             }
+        }
+
+        /// <summary>
+        /// Apply an action for each element of the sequence and returns new processed sequence.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of <see cref="source"/>.</typeparam>
+        /// <param name="source">An <see cref="IEnumerable{T}"/> that contains the elements to apply the action to.</param>
+        /// <param name="action">A function to affect each element of the sequence. Returns new processed element.</param>
+        /// <returns>New sequence with processed elements.</returns>
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Func<T, T> action)
+        {
+            var collection = new Collection<T>();
+
+            foreach (var item in source)
+            {
+                collection.Add(action(item));
+            }
+
+            return collection;
         }
     }
 }
