d9f7adecce2383f06f84ffc43f9ca7762d1a1dbe
iivchenko <ivan.ivchenko@softheme.com>
2015-05-10 16:43:40 +0300

    Extend EnumerableExtensions
    Add void ForEach<T>(this IEnumerable<T> source, Action<T> action)

--- Shogun.Utility.Tests/Extensions/EnumerableExtensionsTests.cs
+++ Shogun.Utility.Tests/Extensions/EnumerableExtensionsTests.cs
@@ -4,12 +4,22 @@
 // <author>Ivan Ivchenko</author>
 // <email>iivchenko@live.com</email>
 
+using System.Collections.Generic;
 using NUnit.Framework;
+using Shogun.Utility.Extensions;
 
 namespace Shogun.Utility.Tests.Extensions
 {
     [TestFixture]
     public class EnumerableExtensionsTests
     {
+        [Test]
+        public void ForEach_Test()
+        {
+            var counter = 1;
+            IEnumerable<int> source = new List<int> { 1, 2, 3, 4, 5 };
+
+            source.ForEach(x => Assert.AreEqual(counter++, x));
+        }
     }
 }
--- Shogun.Utility/Extensions/EnumerableExtensions.cs
+++ Shogun.Utility/Extensions/EnumerableExtensions.cs
@@ -4,6 +4,7 @@
 // <author>Ivan Ivchenko</author>
 // <email>iivchenko@live.com</email>
 
+using System;
 using System.Collections.Generic;
 
 namespace Shogun.Utility.Extensions
@@ -13,5 +14,18 @@ namespace Shogun.Utility.Extensions
     /// </summary>
     public static class EnumerableExtensions
     {
+        /// <summary>
+        /// Apply an action for each element of the sequence.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of <see cref="source"/>.</typeparam>
+        /// <param name="source">An <see cref="IEnumerable{T}"/> that contains the elements to apply the action to.</param>
+        /// <param name="action">A function to affect each element of the sequence.</param>
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (var item in source)
+            {
+                action(item);
+            }
+        }
     }
 }
