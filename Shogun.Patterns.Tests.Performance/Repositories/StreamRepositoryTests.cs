fd2159987faaf5b44e09affa8e431ab5c6d1a4bb
shogun <iivchenko@live.com>
2015-08-08 15:39:51 +0300

    Create Test class for RepositoryStream

---
+++ Shogun.Patterns.Tests.Performance/Repositories/StreamRepositoryTests.cs
@@ -0,0 +1,15 @@
+﻿// <copyright company="XATA">
+//      Copyright (c) 2015, All Right Reserved
+// </copyright>
+// <author>Ivan Ivchenko</author>
+// <email>iivchenko@live.com</email>
+
+using NUnit.Framework;
+
+namespace Shogun.Patterns.Tests.Performance.Repositories
+{
+    [TestFixture]
+    public class StreamRepositoryTests
+    {
+    }
+}
